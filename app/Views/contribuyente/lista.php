15827ff5a45dbf3ecbf47ff7e39a20eb6227116c
70167122 <elvisbravo262@gmail.com>
2025-04-18 10:59:11 -0500

    utf8mb4_general_ci codeigniter 4

--- app/Views/contribuyente/lista.php
+++ app/Views/contribuyente/lista.php
@@ -492,7 +492,6 @@
 
             </div>
             <div class="modal-footer">
-                >
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
             </div>
         </div>
