9182db635885e355cd63a6026e06aa5726d6d126
grupoes <desarrollo.tecnologico.tarapoto@gmail.com>
2025-07-16 11:28:21 -0500

    caja diaria cajero y delete la caja de tarapoto

--- app/Views/caja/cajero.php
+++ app/Views/caja/cajero.php
@@ -5,7 +5,7 @@
 <div class="pc-content">
 
     <div class="row">
-        <div class="col-md-6">
+        <div class="col-md-6 mx-auto">
             <div class="card">
                 <div class="card-header">
                     <h5 class="mb-2">Estado de la caja hoy</h5>
@@ -74,77 +74,7 @@
             </div>
         </div>
 
-        <div class="col-md-6">
-            <div class="card">
-                <div class="card-header">
-                    <h5 class="mb-2">Caja Grupo ESconsultores (Tarapoto)</h5>
-                </div>
-                <div class="card-body">
-                    <div class="mb-4">
-                        <span style="font-size: 18px">Estado de la caja Grupo ESconsultores</span>
-                    </div>
-                    <div class="row g-4">
-                        <div class="col-md-12">
-                            <ul class="list-group">
-                                <li class="list-group-item d-flex justify-content-between align-items-start">
-                                    <div class="ms-2 me-auto">
-                                        <div>Ingresos Caja Fisica</div>
-                                    </div>
-                                    <span class="badge bg-primary rounded-pill">S/ 14.00</span>
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-start">
-                                    <div class="ms-2 me-auto">
-                                        <div>Egresos Caja Fisica</div>
-                                    </div>
-                                    <span class="badge bg-danger rounded-pill">S/ 14.00</span>
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-start">
-                                    <div class="ms-2 me-auto">
-                                        <div class="fw-bold">Utilidad Caja Fisica</div>
-                                    </div>
-                                    <span class="badge bg-success rounded-pill">S/ 14.00</span>
-                                </li>
 
-                                <li class="list-group-item d-flex justify-content-between align-items-start">
-                                    <div class="ms-2 me-auto">
-                                        <div>Ingresos Caja virtual</div>
-                                    </div>
-                                    <span class="badge bg-primary rounded-pill">S/ 14.00</span>
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-start">
-                                    <div class="ms-2 me-auto">
-                                        <div>Egresos Caja Virtual</div>
-                                    </div>
-                                    <span class="badge bg-danger rounded-pill">S/ 14.00</span>
-                                </li>
-                                <li class="list-group-item d-flex justify-content-between align-items-start">
-                                    <div class="ms-2 me-auto">
-                                        <div class="fw-bold">Utilidad Caja Virtual</div>
-                                    </div>
-                                    <span class="badge bg-success rounded-pill">S/ 14.00</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="card-footer">
-                    <div class="row g-2">
-                        <div class="col-md-6">
-                            <div class="d-grid">
-                                <button class="btn btn-outline-secondary d-grid"><span class="text-truncate w-100">Saldo en Caja</span></button>
-                            </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="d-grid">
-                                <button class="btn btn-primary d-grid"><span class="text-truncate w-100">S/ 100.00</span></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-        </div>
     </div>
 
 </div>
