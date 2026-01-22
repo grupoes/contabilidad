0c1f17c709c08e9134558972bee637f5ec57766c
grupoes <desarrollo.tecnologico.tarapoto@gmail.com>
2026-01-22 12:23:25 -0500

    ruc 10008599055

--- app/Controllers/Api/Auth.php
+++ app/Controllers/Api/Auth.php
@@ -13,98 +13,110 @@ class Auth extends ResourceController
 
     public function login()
     {
-        $data = $this->request->getJSON(true);
-
-        if (!isset($data['username']) || !isset($data['password'])) {
-            return $this->respond([
-                'status' => false,
-                'message' => 'Usuario y contraseña son requeridos'
-            ], 400);
-        }
-
-        if (strlen($data['username']) == 11) {
-            $contri = new ContribuyenteModel();
-
-            $user = $contri->where('ruc', $data['username'])->first();
-
-            if (!$user) {
-                return $this->respond([
-                    'status' => false,
-                    'message' => 'Usuario no encontrado'
-                ], 404);
-            }
-
-            if ($data['password'] !== $user['acceso']) {
-                return $this->respond([
-                    'status' => false,
-                    'message' => 'Contraseña incorrecta'
-                ], 401);
-            }
-
-            // Crear JWT
-            $key = getenv('JWT_SECRET');
-            $payload = [
-                'iat' => time(),             // Fecha emisión
-                'exp' => time() + 3600,      // Expira en 1 hora
-                'uid' => $user['id'],        // ID de usuario
-                'username' => $user['ruc'],
-                'role' => 'contribuyente'
-            ];
-
-            $token = JWT::encode($payload, $key, 'HS256');
+        try {
+            $data = $this->request->getJSON(true);
 
             return $this->respond([
                 'status' => true,
-                'message' => 'Login exitoso',
-                'token' => $token,
-                'user' => [
-                    'id' => $user['id'],
-                    'nombre' => $user['razon_social'],
+                'message' => $data
+            ]);
+
+            if (!isset($data['username']) || !isset($data['password'])) {
+                return $this->respond([
+                    'status' => false,
+                    'message' => 'Usuario y contraseña son requeridos'
+                ], 400);
+            }
+
+            if (strlen($data['username']) == 11) {
+                $contri = new ContribuyenteModel();
+
+                $user = $contri->where('ruc', $data['username'])->first();
+
+                if (!$user) {
+                    return $this->respond([
+                        'status' => false,
+                        'message' => 'Usuario no encontrado'
+                    ], 404);
+                }
+
+                if ($data['password'] !== $user['acceso']) {
+                    return $this->respond([
+                        'status' => false,
+                        'message' => 'Contraseña incorrecta'
+                    ], 401);
+                }
+
+                // Crear JWT
+                $key = getenv('JWT_SECRET');
+                $payload = [
+                    'iat' => time(),             // Fecha emisión
+                    'exp' => time() + 3600,      // Expira en 1 hora
+                    'uid' => $user['id'],        // ID de usuario
                     'username' => $user['ruc'],
                     'role' => 'contribuyente'
-                ]
-            ]);
-        } else {
-            $userModel = new TrabajadoresContriModel();
-            $user = $userModel->where('numero_documento', $data['username'])->first();
+                ];
 
-            if (!$user) {
+                $token = JWT::encode($payload, $key, 'HS256');
+
                 return $this->respond([
-                    'status' => false,
-                    'message' => 'Usuario no encontrado'
-                ], 404);
-            }
+                    'status' => true,
+                    'message' => 'Login exitoso',
+                    'token' => $token,
+                    'user' => [
+                        'id' => $user['id'],
+                        'nombre' => $user['razon_social'],
+                        'username' => $user['ruc'],
+                        'role' => 'contribuyente'
+                    ]
+                ]);
+            } else {
+                $userModel = new TrabajadoresContriModel();
+                $user = $userModel->where('numero_documento', $data['username'])->first();
 
-            if ($data['password'] !== $user['password']) {
-                return $this->respond([
-                    'status' => false,
-                    'message' => 'Contraseña incorrecta'
-                ], 401);
-            }
+                if (!$user) {
+                    return $this->respond([
+                        'status' => false,
+                        'message' => 'Usuario no encontrado'
+                    ], 404);
+                }
 
-            // Crear JWT
-            $key = getenv('JWT_SECRET');
-            $payload = [
-                'iat' => time(),             // Fecha emisión
-                'exp' => time() + 3600,      // Expira en 1 hora
-                'uid' => $user['id'],        // ID de usuario
-                'username' => $user['numero_documento'],
-                'role' => 'trabajador'
-            ];
+                if ($data['password'] !== $user['password']) {
+                    return $this->respond([
+                        'status' => false,
+                        'message' => 'Contraseña incorrecta'
+                    ], 401);
+                }
 
-            $token = JWT::encode($payload, $key, 'HS256');
-
-            return $this->respond([
-                'status' => true,
-                'message' => 'Login exitoso',
-                'token' => $token,
-                'user' => [
-                    'id' => $user['id'],
-                    'nombre' => $user['nombres'],
+                // Crear JWT
+                $key = getenv('JWT_SECRET');
+                $payload = [
+                    'iat' => time(),             // Fecha emisión
+                    'exp' => time() + 3600,      // Expira en 1 hora
+                    'uid' => $user['id'],        // ID de usuario
                     'username' => $user['numero_documento'],
                     'role' => 'trabajador'
-                ]
-            ]);
+                ];
+
+                $token = JWT::encode($payload, $key, 'HS256');
+
+                return $this->respond([
+                    'status' => true,
+                    'message' => 'Login exitoso',
+                    'token' => $token,
+                    'user' => [
+                        'id' => $user['id'],
+                        'nombre' => $user['nombres'],
+                        'username' => $user['numero_documento'],
+                        'role' => 'trabajador'
+                    ]
+                ]);
+            }
+        } catch (\Exception $e) {
+            return $this->respond([
+                'status' => false,
+                'message' => 'No se puede hacer la consulta ' . $e->getMessage()
+            ], 500);
         }
     }
 }
