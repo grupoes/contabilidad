fe04eff235cf6b25d618affd0c0545e62eb9d295
grupoes <desarrollo.tecnologico.tarapoto@gmail.com>
2026-09-17 11:25:34 -0500

    ffds

--- app/Controllers/Api/Notificaciones.php
+++ app/Controllers/Api/Notificaciones.php
@@ -2156,7 +2156,7 @@ class Notificaciones extends ResourceController
             'titulo'        => $titulo,
             'contenido'     => $contenido,
             'fechaCreacion' => date('Y-m-d H:i:s'),
-            'creadoPor'     => 0,
+            'creadoPor'     => null,
             'typeContri'    => $data['typeContri'] ?? 'otro',
             'envio_file'    => $data['envio_file'] ?? 'NO',
             'estado'        => 1,
